cb2cd2167adf896a0dcf1420f1afec00ef692d49
deepagp <41198029+deepagp@users.noreply.github.com>
2019-11-18 16:26:50 +0530

    Update function_call_by_ref.c

--- C_pgmz/function_call_by_ref.c
+++ C_pgmz/function_call_by_ref.c
@@ -32,9 +32,9 @@ int main () {
 void swap(int *x, int *y) {
 
    int temp;
-   temp = *x;    /* save the value at address x */
-   *x = *y;      /* put y into x */
-   *y = temp;    /* put temp into y */
+//    temp = *x;    /* save the value at address x */
+//    *x = *y;      /* put y into x */
+//    *y = temp;    /* put temp into y */
   
    return;
 }

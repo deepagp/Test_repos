860979cdb730a22713eaad84d5baa3310d8b3595
deepagp <41198029+deepagp@users.noreply.github.com>
2019-11-18 15:52:49 +0530

    Update sample.cmake

--- C_pgmz/sample.cmake
+++ C_pgmz/sample.cmake
@@ -1,32 +1,32 @@
 cmake_minimum_required(VERSION 2.6)
-	add_executable(	arithmetic arithmetic.c,
-relationop relationop.c,
-logical logical.c,
-bitwiseop bitwiseop.c,
-shiftop shiftop.c,
-assignmentop assignmentop.c,
-miscelleneousoperator miscelleneousoperator.c,
-if_stmt if_stmt.c,
-if_else if_else.c,
-nested_if nested_if.c,
-switch switch.c,
-nested_switch nested_switch.c,
-condition_exprssion condition_exprssion.c,
-for for.c,
-while while.c,
-do_while do_while.c,
-jumpst jumpst.c,
-goto goto.c,
-return return.c,
-functions functions.c,
-function_pointer function_pointer.c,
-recursion recursion.c,
-function_call_by_value function_call_by_value.c,
-function_call_by_ref function_call_by_ref.c,
-union union .c,
-structure structure.c,
-arrays arrays.c,
-enum enum.c,
-storage_class storage_class.c,
-typedef typedef.c,
+	add_executable(	arithmetic arithmetic.c 
+relationop relationop.c 
+logical logical.c 
+bitwiseop bitwiseop.c 
+shiftop shiftop.c 
+assignmentop assignmentop.c 
+miscelleneousoperator miscelleneousoperator.c 
+if_stmt if_stmt.c 
+if_else if_else.c 
+nested_if nested_if.c 
+switch switch.c 
+nested_switch nested_switch.c 
+condition_exprssion condition_exprssion.c 
+for for.c 
+while while.c 
+do_while do_while.c 
+jumpst jumpst.c 
+goto goto.c 
+return return.c 
+functions functions.c 
+function_pointer function_pointer.c 
+recursion recursion.c 
+function_call_by_value function_call_by_value.c 
+function_call_by_ref function_call_by_ref.c 
+union union .c 
+structure structure.c 
+arrays arrays.c 
+enum enum.c 
+storage_class storage_class.c 
+typedef typedef.c 
 malloc_realloc malloc_realloc.c	)
